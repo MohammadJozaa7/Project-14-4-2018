1357d505808a08221289a28a4028e2e707f4b143
MohammadJozaa7 <36266902+MohammadJozaa7@users.noreply.github.com>
2018-04-14 14:52:44 +0300

    before connect the server

---
+++ STEPS.js
@@ -0,0 +1,92 @@
+/*
+the idea:
+1- front end: client folder (Angular)
+2- back end: server folder (Express)
+3- data base: database folder (Mongoodb)
+
+1- must make folder / files 
+ server: server.js
+ client
+ db
+ index.html (main page)
+
+2- make our npm test file ready (package.json)
+  by run command (npm init --yes)
+
+3- put all thing we need inside (package.json)
+  by run the command (npm install <package_name>)
+  or with (--save) to save it on the file (package.json)
+  so we need: express / angular / mongodb
+  and we will see it in package.json 
+
+4- go to the index and make the basic thing on our page
+  some change in the html to see the thing
+5- now connect the index with the server and open the server
+
+6- 
+
+7- 
+
+8- 
+
+9- 
+
+10- 
+
+11- 
+
+12- 
+
+13- 
+
+14- 
+
+15- 
+
+16- 
+
+17- 
+
+18- 
+
+19- 
+
+20- 
+
+21- 
+
+22- 
+
+23- 
+
+24-
+
+25- 
+
+
+advance step:
+1- do the huroku thing
+2- the deployment
+3- the shortly
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
